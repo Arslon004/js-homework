0d99accf4956859254a86741c1e857857b211fc4
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 22:16:45 +0300

    if4.js

---
+++ if/if4.js
@@ -0,0 +1,11 @@
+let x=Number(prompt("x sonini kiriting"));
+let y=Number(prompt("y sonini kiriting"));
+if(x>0 && y >0){
+  console.log("1-chorak");
+}else if(x<0 && y>0){
+  console.log("2-chorak");
+}else if(x<0 && y<0){
+  console.log("3-chorak");
+}else if(x>0 && y<0){
+  console.log("4-chorak");
+}
