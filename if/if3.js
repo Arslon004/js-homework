c12bc935f17326bb772104a3e2d936609a0f07f7
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 22:10:54 +0300

    if3.js

---
+++ if/if3.js
@@ -0,0 +1,10 @@
+let a=Number(prompt("1-sonni kiriting"));
+let b=Number(prompt("2-sonni kiriting"));
+if(a>b){
+  console.log(a);
+}else if(a<b){
+  console.log(b)
+}
+else if(a==b){
+  console.log(0)
+}
