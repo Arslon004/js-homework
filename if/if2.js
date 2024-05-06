5d4e1cfad02c2a6d4687d6a51a012f0798b06eb8
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 18:49:04 +0300

    if2.js

---
+++ if/if2.js
@@ -0,0 +1,5 @@
+let a=Number(prompt("0 dan 1000 gacha bo'lgan sonlarni kiriting"));
+let b=Number(prompt("0 dan 1000 gacha bo'lgan sonlarni kiriting"));
+let natija =(1/6*(Math.sqrt(a))+1/3*(Math.sqrt(b)));
+let result=Math.pow(natija,2);
+console.log(result);
