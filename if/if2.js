4292d01b673a55f8a3ffaba59d151a636ad075d3
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 18:14:08 +0300

    initial commit

--- if/if2.js
+++ if/if2.js
@@ -1,4 +1,5 @@
 let a=Number(prompt("0 dan 1000 gacha bo'lgan raqam kiriting"));
 let b=Number(prompt("0 dan 1000 gacha bo'lgan raqam kiriting"));
-let natija=Math.sqrt(Math.sqrt(((Math.pow(a,3))-(Math.pow(b,3)))+((Math.pow(a,3))+(Math.pow(b,3)))));
-console.log(natija);
+let natija=(1/6*(Math.sqrt(a))+1/3*(Math.sqrt(b)));
+let result=Math.pow(natija,2);
+console.log(result);
