870601c2e50fa7c4f7b8f44511d29e64b076825e
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-07 00:07:33 +0300

    boolean7.js

---
+++ boolean/boolean7.js
@@ -0,0 +1,4 @@
+let a = Number(prompt("1-sonni kiriting"));
+let b = Number(prompt("2-sonni kiriting"));
+let c = Number(prompt("2-sonni kiriting"));
+b >= a && b <= c ? console.log("True") : console.log("False");
