ff91cb7c5f66616c72e77cfe10df7d9a811d75a9
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 18:36:42 +0300

    initial commit

---
+++ if/if1.js
@@ -0,0 +1,4 @@
+let a=Number(prompt("0 dan 1000 gacha bo'lgan sonlarni kiriting"));
+let b=Number(prompt("0 dan 1000 gacha bo'lgan sonlarni kiriting"));
+let result=Math.sqrt(Math.sqrt(((Math.pow(a,3))-(Math.pow(b,3)))+((Math.pow(a,3))+(Math.pow(b,3)))));
+console.log(result);
--- if/if2.js
+++
@@ -1,5 +0,0 @@
-let a=Number(prompt("0 dan 1000 gacha bo'lgan raqam kiriting"));
-let b=Number(prompt("0 dan 1000 gacha bo'lgan raqam kiriting"));
-let natija=(1/6*(Math.sqrt(a))+1/3*(Math.sqrt(b)));
-let result=Math.pow(natija,2);
-console.log(result);
