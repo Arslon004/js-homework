49332d85059cdf4fc45273d6b71d59a52f2c425e
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 22:25:16 +0300

    if5.js

---
+++ if/if5.js
@@ -0,0 +1,9 @@
+let x=Number(prompt("x sonni kiriting"));
+let sinx=Math.sin(x*Math.PI/180);
+if(x>0){
+  let result=2*sinx;
+  console.log(result)
+}else {
+  let result=x-6;
+  console.log(result);
+}
