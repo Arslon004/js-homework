0b678b6f31c1d1878df1afb96016bced1d2880a2
Arslon004 <arslonbozorov2109@gmail.com>
2024-05-06 21:45:22 +0300

    if1.js

---
+++ if/if1.js
@@ -0,0 +1,10 @@
+let a=Number(prompt("1-sonni kiriting"));
+let b=Number(prompt("2-sonni kiriting"));
+let c=Number(prompt("3-sonni kiriting"));
+engKichik=a
+if(b<engKichik){
+ engKichik=b
+}else if(c<engKichik){
+ c=engKichik;
+}
+console.log(engKichik)
